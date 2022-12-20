8a762dcdd2fd2f1c6e7c0ffa7e550f87f576223b
thakoraarya <aarya191102@gmail.com>
2022-12-20 11:11:08 +0530

    portfolio-s3

--- pages/components/DivOne.js
+++ pages/components/DivOne.js
@@ -19,8 +19,16 @@ const DivOne = () => {
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque tempora iste eum, repellendus tenetur hic quo molestias facere ex laborum nisi sed porro quam saepe ab. Maiores quidem quasi est?
                 </p>
             </div>
+            {/* social media links and my memoji  */}
+            <LinkNMemoji />
 
-            <LinkNMemoji/>
+            <section>
+                <h1 className='text-center text-3xl my-5 mx-auto font-sans'>What I've done?</h1>
+                <p className='text-justify text-sm font-typewriter'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis soluta ab, quaerat facere tempore magni eaque vero quis nemo sint officia natus ipsa facilis repellendus saepe, eos temporibus nostrum aliquid dolores! Vel!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis soluta ab, quaerat facere tempore magni eaque vero quis nemo sint officia natus ipsa facilis repellendus saepe, eos temporibus nostrum aliquid dolores! Vel!
+                     </p>
+            </section>
         </>
     )
 }
--- pages/components/LinkNMemoji.js
+++ pages/components/LinkNMemoji.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { BsYoutube, BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs'
-import Image from 'next/image'
-// import aarya from "/Users/thakoraarya/Documents/clg codes PU/MERN/rectjs/portfolio/pages/myMemoji.jpeg"
-import aarya from '/Users/thakoraarya/Documents/clg codes PU/MERN/rectjs/portfolio/public/myMemoji.jpeg'
 
 
 const LinkNMemoji = () => {
@@ -15,9 +12,8 @@ const LinkNMemoji = () => {
         <BsLinkedin />
         <BsTwitter />
       </div>
-      <div className='relative bg-gradient-to-b from-teal-400 rounded-full w-80 h-80 mt-20 mx-auto'>
-        {/* <img src="myMemoji.png" alt="my memoji" layout='fill' object-fit="cover"/> */}
-     <Image src="/myMemoji.png" width="64" height="64" />
+      <div className='relative w-80 mt-20 mx-auto'>
+        <img src="myMemoji.png"  alt="my memoji"/>
       </div>
     </>
   )
